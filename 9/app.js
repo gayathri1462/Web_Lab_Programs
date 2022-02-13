b2e9fa2f47008502f2d90b8268a87dcf07f5a49e
Gayathri Devi Nagalapuram <gayathri1462@gmail.com>
2022-02-13 13:21:48 +0530

    Update app.js

--- 9/app.js
+++ 9/app.js
@@ -1,21 +1,32 @@
-var express = require("express");
-var path = require("path");
-var MongoClient = require("mongodb").MongoClient;
+var express = require('express');
+var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
-var url = "mongodb://localhost:27017/";
 var app = express();
-MongoClient.connect(url, (err, database) => {
+//enter the name of the database in the end 
+var url = "mongodb://localhost:27017/";
+var dbo;
+MongoClient.connect(url, function(err, database) {
     if (err) throw err;
-    var dbo = database.db("signup");
-    console.log("created db");
-    app.get("/", (req, res) => {
-        return res.redirect("/public/index.html");
+    dbo = database.db("signup");
+    console.log("database created successfully");
+
+
+    app.get('/', function(req, res) {
+        return res.redirect('/public/index.html');
     }).listen(3000);
-    console.log("port 3000 is created");
-    app.use("/public", express.static(path.join(__dirname , "/public")));
+
+    console.log("Server listening at : 3000");
+
+
+    app.use('/public', express.static(path.join(__dirname, '/public')));
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.post("/success", (req, res) => {
+    app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
+        extended: true
+    }));
+
+    // Sign-up function starts here. . .
+    app.post('/success', function(req, res) {
         var name = req.body.name;
         var email = req.body.email;
         var password = req.body.password;
@@ -40,5 +51,4 @@ MongoClient.connect(url, (err, database) => {
         return res.redirect('/public/success.html');
 
     });
-
 });
